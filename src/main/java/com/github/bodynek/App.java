441b016c4c5f8434beacf6161d9f38a75c660fba
Petr Dittrich <petr.dittrich@wultra.com>
2021-04-24 16:00:37 +0200

    Getting the sheet, row and cell

--- src/main/java/com/github/bodynek/App.java
+++ src/main/java/com/github/bodynek/App.java
@@ -1,13 +1,50 @@
 package com.github.bodynek;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        InputStream file = System.in;
+        try {
+            if (args.length != 0) {
+                file = new FileInputStream(args[0]);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.printf("File %s is not found: \n", args[0]);
+            System.exit(1);
+        }
+        Workbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(file);
+        } catch (IOException e) {
+            System.err.printf("Reading of %s failed\n", file);
+            System.exit(1);
+        }
+        if (workbook.getNumberOfSheets() < 1) {
+            System.err.println("Workbook doesn't contain first sheet");
+            System.exit(1);
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Row row : sheet) {
+            Cell cell = row.getCell(1);
+            if (cell != null) {
+                System.out.println(cell.getAddress());
+            }
+        }
     }
 }
