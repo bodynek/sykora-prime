1d78cf466d53e296e33600c8a1b37efb0b570b87
Petr Dittrich <petr.dittrich@wultra.com>
2021-04-24 14:33:09 +0200

    From archetype

---
+++ src/main/java/com/github/bodynek/App.java
@@ -0,0 +1,13 @@
+package com.github.bodynek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
